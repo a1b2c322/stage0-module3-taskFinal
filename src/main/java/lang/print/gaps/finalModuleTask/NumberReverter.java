1dbaf94fc64e6c53df9d9043f6c35448b5c73115
arastepa <stepanyan2298@gmail.com>
2022-12-20 23:43:30 +0400

    ..

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int res = 0;
+
+        while (number != 0){
+            res = res*10 + number % 10;
+            number = number/10;
+        }
+        System.out.println(res);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double far = 1.8*temperatureCelsius + 32;
+        System.out.println(far);
     }
 }
